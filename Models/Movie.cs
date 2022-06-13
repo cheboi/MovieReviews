93aa9ca114a4c92db0bac1216fc9151d1248326f
cheboi <moscheb96@gmail.com>
2022-06-13 15:12:56 +0300

    initial commit

---
+++ Models/Movie.cs
@@ -0,0 +1,11 @@
+public class Movie
+{
+  public IList<Review> Reviews { get; set; }
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+
+    public void AddReview(Review review)
+    {
+        Reviews.Add(review);
+    }
+} 
---
+++ Models/Review.cs
@@ -0,0 +1,7 @@
+public class Reviews
+{
+    public int Id { get; set; }
+    public Guid MovieId { get; set; }
+    public string Reviewer { get; set; } 
+    public int Stars { get; set; }
+ }
